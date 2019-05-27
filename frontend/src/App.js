e626187870fe0fba49fd953ebd20165e221ff3d6
Elias <eliasbergn96@gmail.com>
2019-05-27 18:50:03 +0200

    Navbar
    Added a constant navbar to help navigation around the app. Currently made in App.js, but may move it.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,25 +3,30 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Todo from "./containers/TODO/Todo";
 import MyTodo from "./containers/MyTodo/MyTodo";
 import Notfound from "./components/notFound";
+import InputForm from "./containers/TODO/components/InputForm";
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Router>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/todos/">My Lists</Link>
-            </li>
-          </ul>
+          <nav>
+            <div class="nav-wrapper">
+              <a href="#!" class="brand-logo center">
+                Nye Uber
+              </a>
+              <ul class="left hide-on-med-and-down">
+                <li>
+                  <Link to="/"> Home</Link>
+                </li>
+                <li>
+                  <Link to="/todos/"> My Lists</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
           <Switch>
-            <Route exact path="/">
-              We can add a homepage or something here. If not we could just
-              redirect to todos.
-            </Route>
+            <Route exact path="/" />
             <Route exact path="/todos" component={Todo} />
             <Route
               exact
@@ -37,3 +42,15 @@ export default class App extends Component {
     );
   }
 }
+
+/* 
+<div class="input-field">
+  <form>
+    <input id="search" type="search" required />
+    <label class="label-icon" for="search">
+      <i class="material-icons">search</i>
+      <i class="material-icons">close</i>
+    </label>
+  </form>
+</div>;
+*/
