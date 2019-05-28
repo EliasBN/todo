c6944256e4dd1a99a91e62887e67daf5304ab33c
Elias <eliasbergn96@gmail.com>
2019-05-28 16:17:12 +0200

    Moved route from App into own component
    Currently also includes styling and navbar. May want to separate these in the future.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,11 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Todo from "./containers/TODO/Todo";
-import MyTodo from "./containers/MyTodo/MyTodo";
-import Notfound from "./components/notFound";
-import Login from "./containers/Login/Login";
+import Routes from "./components/Routes";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Router>
-          <nav>
-            <div class="nav-wrapper">
-              <a href="#!" class="brand-logo center">
-                TODO
-              </a>
-
-              <ul class="left">
-                <li>
-                  <Link to="/"> Home</Link>
-                </li>
-                <li>
-                  <Link to="/todos/"> My Lists</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-
-          <Switch>
-            <Route exact path="/" />
-            <Route exact path="/todos" component={Todo} />
-            <Route
-              exact
-              path="/todos/:id/todoItems/:todoItemId/"
-              component={MyTodo}
-            />
-            <Route path="/todos/:id/todoItems/" component={MyTodo} />
-
-            <Route component={Notfound} />
-          </Switch>
-        </Router>
-        <Login />
+        <Routes />
       </div>
     );
   }
