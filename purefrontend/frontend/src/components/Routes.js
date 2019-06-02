086a0784f1043584d3b06abc3d7b986e75e1d804
Henningermaskin <35727241+Henningermaskin@users.noreply.github.com>
2019-06-02 17:42:55 +0200

    Forgot to delete imports - should work run now (#7)

--- purefrontend/frontend/src/components/Routes.js
+++ purefrontend/frontend/src/components/Routes.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Todo from "../containers/TODO/Todo";
 import MyTodo from "../containers/MyTodo/MyTodo";
 import Notfound from "./notFound";
-import Login from "../containers/Login/Login";
+
 
 export default class Routes extends Component {
   constructor(props) {
